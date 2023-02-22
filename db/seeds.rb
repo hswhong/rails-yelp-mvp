f51b7ac1c5c612a06db31e8e64c977203a923a33
ShiWei Hong <hswhong96@gmail.com>
2023-02-22 13:50:16 +0000

    Challenge 2 done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.create(
+  name: "Koya",
+  address: "London",
+  phone_number: "0123456789",
+  category: "japanese"
+)
+
+Restaurant.create(
+  name: "Wasabi",
+  address: "London",
+  phone_number: "0223456789",
+  category: "japanese"
+)
+
+Restaurant.create(
+  name: "Luciano",
+  address: "London",
+  phone_number: "0323456789",
+  category: "italian"
+)
+
+Restaurant.create(
+  name: "Royal China",
+  address: "London",
+  phone_number: "0423456789",
+  category: "chinese"
+)
+
+Restaurant.create(
+  name: "Plum Valley",
+  address: "London",
+  phone_number: "0523456789",
+  category: "chinese"
+)
